7967002a7a69199debb5cbfee16eb54b09064ef3
DJones2314 <djvjones23@gmail.com>
2019-02-01 15:29:22 +0000

    added joining functionality

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,8 @@
+
+INSERT INTO TRAINER (trainerName) VALUES ('Datt Meamon')
+INSERT INTO TRAINER (trainerName) VALUES ('Prad Bitt')
+INSERT INTO TRAINER (trainerName) VALUES ('Mson Jamoa')
+
 INSERT INTO CLASSROOM (traineeName) VALUES ('Matt Deamon')
 INSERT INTO CLASSROOM (traineeName) VALUES ('Brad Pitt')
 INSERT INTO CLASSROOM (traineeName) VALUES ('Json Mamoa')
--- src/main/resources/import2.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO TRAINER (trainerName) VALUES ('Batt Deamon')
-INSERT INTO TRAINER (trainerName) VALUES ('Jrad Pitt')
-INSERT INTO TRAINER (trainerName) VALUES ('Mson Mamoa')
